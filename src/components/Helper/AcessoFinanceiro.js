5980903dee6ab15c8f56ea5dea96bfbb07635699
L-Cortes <crts99@gmail.com>
2024-10-01 13:08:07 -0300

    Att 01/10

--- src/components/Helper/AcessoFinanceiro.js
+++ src/components/Helper/AcessoFinanceiro.js
@@ -10,7 +10,7 @@ function AcessoFinanceiro() {
     const [imageLoaded, setImageLoaded] = useState(false);
 
     const secoesFinanceiro = [
-        { nome: 'V360', icone: faFileInvoiceDollar, link: 'https://www.appsheet.com/start/894918c5-7548-431d-96c0-5c1f2f0a51a0#appName=TicketsGeral-448944302&view=Novo%20Ticket&defaults={%22Categoria%22:%22V360%22}' }
+        { nome: 'V360', icone: faFileInvoiceDollar, link: '#https://www.appsheet.com/start/894918c5-7548-431d-96c0-5c1f2f0a51a0#appName=TicketsGeral-448944302&view=Novo%20Ticket&defaults={%22Categoria%22:%22V360%22}' }
     ];
 
     useEffect(() => {
